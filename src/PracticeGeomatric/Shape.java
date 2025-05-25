adc98479f2b3958dcd066103f3dd16d49d53c8de
Ryo-N-0106 <ntkn717416@gmail.com>
2025-05-25 09:44:35 +0700

    BT Thuc hanh_Đối tượng hình học

---
+++ src/PracticeGeomatric/Shape.java
@@ -0,0 +1,30 @@
+package PracticeGeomatric;
+
+public class Shape {
+    public String color = "green";
+    public boolean filled = true;
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public Shape() {
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+}
